2a227d570e13f0122884d69503a71e4393065649
oli700951-svg <oli700951@gmail.com>
2025-11-27 22:28:02 -0600

    Update database.py

--- app/database.py
+++ app/database.py
@@ -5,11 +5,9 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
-# Solo aplicar sslmode para Postgres
 if DATABASE_URL.startswith("postgres"):
     DATABASE_URL += "?sslmode=require"
 
-# Crear engine
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
@@ -18,7 +16,6 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependencia para FastAPI
 def get_db():
     db = SessionLocal()
     try:
